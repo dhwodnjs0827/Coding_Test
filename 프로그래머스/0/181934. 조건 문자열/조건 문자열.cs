7f47ab6116750671e870faffe13460176ceaa47d
Jaewon <dhwodnjs0827@naver.com>
2026-06-09 10:36:15 +0900

    [level 0] Title: 조건 문자열, Time: 0.33 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Solution {
+    public int solution(string ineq, string eq, int n, int m) {
+        int answer = 0;
+        
+        if (ineq.Equals(">"))
+        {
+            if (eq.Equals("="))
+            {
+                answer = n >= m ? 1 : 0;
+            }
+            else
+            {
+                answer = n > m ? 1 : 0;
+            }
+        }
+        else
+        {
+            if (eq.Equals("="))
+            {
+                answer = n <= m ? 1 : 0;
+            }
+            else
+            {
+                answer = n < m ? 1 : 0;
+            }
+        }
+        
+        return answer;
+    }
+}
