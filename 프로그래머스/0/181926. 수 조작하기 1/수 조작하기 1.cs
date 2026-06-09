fea94a31ed143355243355af8bd8d9faa5b1f595
Jaewon <dhwodnjs0827@naver.com>
2026-06-09 10:37:55 +0900

    [level 0] Title: 수 조작하기 1, Time: 1.30 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.cs
@@ -0,0 +1,25 @@
+using System;
+
+public class Solution {
+    public int solution(int n, string control) {
+        foreach (var c in control)
+        {
+            switch (c)
+            {
+                case 'w':
+                    n++;
+                    break;
+                case 's':
+                    n--;
+                    break;
+                case 'd':
+                    n += 10;
+                    break;
+                case 'a':
+                    n -= 10;
+                    break;
+            }
+        }
+        return n;
+    }
+}
