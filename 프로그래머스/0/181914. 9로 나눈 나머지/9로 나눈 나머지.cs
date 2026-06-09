2bbf279032af1369aa721b5d0f44b346b71f3184
Jaewon <dhwodnjs0827@naver.com>
2026-06-09 10:40:24 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 10.66 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution
+{
+    public int solution(string number)
+    {
+        int answer = 0;
+        for (var i = 0; i < number.Length; i++)
+        {
+            answer += int.Parse(number[i].ToString());
+        }
+        return answer % 9;
+    }
+}
