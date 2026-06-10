0f22f510ae0d33f05bfef2271aaa9f8459addc05
Jaewon <dhwodnjs0827@naver.com>
2026-06-10 13:52:17 +0900

    [level 0] Title: 1로 만들기, Time: 0.19 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class Solution
+{
+    public int solution(int[] num_list)
+    {
+        int answer = 0;
+        for (int i = 0; i < num_list.Length; i++)
+        {
+            while (num_list[i] != 1)
+            {
+                answer++;
+                if (num_list[i] % 2 == 0)
+                {
+                    num_list[i] /= 2;
+                }
+                else
+                {
+                    num_list[i] = (num_list[i] - 1) / 2;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
