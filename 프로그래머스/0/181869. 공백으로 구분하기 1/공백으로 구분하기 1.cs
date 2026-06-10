3968e646985f182cce90a655e0aa03da2c0432c1
Jaewon <dhwodnjs0827@naver.com>
2026-06-10 13:53:49 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 1.98 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution
+{
+    public string[] solution(string my_string)
+    {
+        string[] answer = null;
+        answer = my_string.Split(' ');
+        return answer;
+    }
+}
