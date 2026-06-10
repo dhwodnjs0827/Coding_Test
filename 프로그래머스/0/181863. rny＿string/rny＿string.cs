0127698c5cf49f252ae9a067de286c0768cafc7b
Jaewon <dhwodnjs0827@naver.com>
2026-06-10 13:54:38 +0900

    [level 0] Title: rny_string, Time: 0.49 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution
+{
+    public string solution(string rny_string)
+    {
+        string answer = "";
+        for (int i = 0; i < rny_string.Length; i++)
+        {
+            if (rny_string[i].Equals('m'))
+            {
+                answer += "rn";
+            }
+            else
+            {
+                answer += rny_string[i];
+            }
+        }
+        return answer;
+    }
+}
