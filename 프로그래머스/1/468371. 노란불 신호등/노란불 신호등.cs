1d0ee3ba2c260ee6c9ef631f58b85d220dfe628c
Jaewon <dhwodnjs0827@naver.com>
2026-06-26 11:58:57 +0900

    [level 1] Title: 노란불 신호등, Time: 3.80 ms, Memory: 45.8 MB -BaekjoonHub

--- 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cs
+++ 프로그래머스/1/468371. 노란불 신호등/노란불 신호등.cs
@@ -2,15 +2,19 @@ public class Solution
     {
         public int solution(int[,] signals)
         {
+            // 신호등 갯수
             int n = signals.GetLength(0);
 
+            // 모든 신호등의 주기
             int[] periods = new int[n];
             for (int i = 0; i < n; i++)
             {
+                // 신호등 주기 = 초록불 시간 + 노란불 시간 + 빨간불 시간
                 periods[i] = signals[i, 0] + signals[i, 1] + signals[i, 2];
             }
 
-            long totalLCM = 1L;
+            // 모든 신호등 주기의 최소공배수
+            long totalLCM = 1;
             for (int i = 0; i < n; i++)
             {
                 totalLCM = LCM(totalLCM, periods[i]);
@@ -36,7 +40,10 @@ public class Solution
             
             return -1;
         }
-
+        
+        /// <summary>
+        /// 특정 신호등의 노란불 여부
+        /// </summary>
         private bool IsYellow(int[,] signals, int i, long t)
         {
             int Green = signals[i, 0];
