7c678219c4bddcac7c8ec4a24f20933881580cd8
Jaewon <dhwodnjs0827@naver.com>
2026-07-31 12:07:42 +0900

    [level 1] Title: 콜라 문제, Time: 0.13 ms, Memory: 46.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution
+{
+    public int solution(int a, int b, int n)
+    {
+        int answer = 0;
+
+        while (n >= a)
+        {
+            int newCola = n / a * b;
+            answer += newCola;
+            n = (n % a) + newCola;
+        }
+        
+        return answer;
+    }
+}
